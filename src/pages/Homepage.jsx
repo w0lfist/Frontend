3f3fbee671a40c2f0c109f5a5b27990f2383a06f
w0lfist <165479301+w0lfist@users.noreply.github.com>
2024-09-24 14:19:43 -0500

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Login from "./Login";
+import RedirectionButton from "../components/redirection Button";
 
 function Homepage() {
     return (
         <div>Homepage
+            <div id=""><RedirectionButton ruta="/user/new" texto="registrate" /></div>
+           
             <Login />
         </div>
+        
     );
 }
 
